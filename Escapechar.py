2d005db8c99231d7461862967d80bd45b6b1f47d
Sujoy Halder <sujoy.sh9830@gmail.com>
2020-05-25 14:14:48 +0530

    First Commit

---
+++ Escapechar.py
@@ -0,0 +1,31 @@
+splitstring = "This string bas been\nsplit over\nseveral\nlines"
+print(splitstring)
+#or
+anothersplitstring = """This string has been
+split over
+several
+lines"""
+print(anothersplitstring)
+
+anothersplitstring = """This string has been \
+split over \
+several \
+lines"""
+print(anothersplitstring)
+
+tabbedstring = "1\t2\t3\t4\t5"
+print(tabbedstring)
+
+print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
+#or
+print("The pet shop owner said \"No, no, 'e's uh,...he's resting\".")
+#or
+print("""The pet shop owner said "NO, no, 'e's uh,...he's resting".""")
+#or
+print("""The pet shop owner said "NO, no, \
+'e's uh,...he's resting".""")
+
+print("c:\\users\\timbuchalka\\nots.txt")
+#or
+print(r"c:\users\timbuchalka\notes.txt")
+
---
+++ Strings.py
@@ -0,0 +1,10 @@
+print("Today is a good day")
+print('To learn python')
+print("python's is a easy programming language")
+print('Here is some "logics" function')
+print("Hello"+"World")
+gretting="Hello"
+name="Sujoy"
+print(gretting+' '+name)
+name=input("Enter your name: ")
+print(gretting+' '+name)
